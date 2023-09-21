e198a463cd98364a2c7b32a9688bba0239507c75
EdwardGold Edem Hogan <edge37@outlook.com>
2023-09-21 10:25:03 +0000

    .

---
+++ index.js
@@ -0,0 +1,22 @@
+import { pipeline } from "@xenova/transformers";
+import { koaBody } from "koa-body";
+import koa from "koa";
+
+const extractor = await pipeline(
+  "feature-extraction",
+  "Xenova/bert-base-uncased",
+  { revision: "default" }
+);
+
+new koa()
+  .use(koaBody())
+  .use(async (ctx) => {
+    const text = await ctx.request.body;
+    ctx.body = (
+      await extractor(text, { pooling: "mean", normalize: true })
+    ).data;
+  })
+  .listen(
+    10000,
+    process.env.NODE_ENV === "production" ? "0.0.0.0" : "127.0.0.1"
+  );
